ea7c896c056662c71da4052bfdc1d32275681a67
NyxSkye <adititewari17@gmail.com>
2026-09-24 10:39:14 +0530

    Base Layout

---
+++ js/game.js
@@ -0,0 +1,14 @@
+import { Snake } from './snake.js';
+
+export class Game {
+    constructor() {
+        this.rows = 20;
+        this.columns = 20;
+        //create snake 
+        this.snake = new Snake();
+        //craete food
+    }
+    //set direction
+    //update the game state
+    // end game
+}
