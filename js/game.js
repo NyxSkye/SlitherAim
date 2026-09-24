7929925eb3467842e45f0e3b1a43a7c2a5d46824
NyxSkye <adititewari17@gmail.com>
2026-09-24 12:22:53 +0530

    making snake move and add buttons

--- js/game.js
+++ js/game.js
@@ -1,14 +1,80 @@
 import { Snake } from './snake.js';
 
 export class Game {
+
     constructor() {
+
         this.rows = 20;
+
         this.columns = 20;
+
         //create snake 
         this.snake = new Snake();
+
         //craete food
+        // Set the initial speed in milliseconds, direction to "RIGHT"
+        //because the snake starts moving to the right, and nextDirection to "RIGHT"
+        // because the snake starts moving to the right
+        this.speed = 150;
+        this.direction = "RIGHT";
+        this.nextDirection = "RIGHT";
+        
+        this.running = true;
+        
+
     }
     //set direction
-    //update the game state
+    setDirection(direction) {
+
+        const opposite = { // Define opposite directions
+
+            UP: "DOWN",
+            DOWN: "UP",
+            LEFT: "RIGHT",
+            RIGHT: "LEFT"
+
+        };
+
+        if (opposite[this.direction] === direction) {
+            return;
+        }
+
+        this.nextDirection = direction;
+
+    }
     // end game
+    //update the game state
+    update() {
+
+        if (this.running === false) return;
+
+        this.direction = this.nextDirection;
+
+        const movement = {
+
+            UP: { x: 0, y: -1 },
+            DOWN: { x: 0, y: 1 },
+            LEFT: { x: -1, y: 0 },
+            RIGHT: { x: 1, y: 0 }
+        };
+
+        const head = this.snake.getHead();
+
+        const newHead = {
+            x:
+                head.x +
+                movement[this.direction].x,
+
+            y:
+                head.y +
+                movement[this.direction].y
+
+        };
+
+           this.snake.move(newHead);
+           this.snake.removeTail();
+
+    }
+    
+
 }
